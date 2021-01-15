35e1d86a6c0e3ff695ce75383280a69353c72bde
unknown <digomic@outlook.es>
2021-01-15 06:56:23 -0600

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri:  " https://limitless-sands-05811.herokuapp.com/ "
 });
 
 const authLink = setContext(() => {
